cbe904f571bfb6a21d390ea092168ad5bf96379f
Paulina Gonzalez C <pgc.lacroix@gmail.com>
2017-08-21 14:17:57 -0300

    validaciones listas de la pantalla ingreseCelular.html

--- assets/js/ingreseCelular_02.js
+++ assets/js/ingreseCelular_02.js
@@ -1,48 +1,49 @@
 $(document).ready(function(){
 
 	// error variables
-	var numberPhoneError = true,
-		EmailError = true;
-
+	var numberPhoneError = true;
 
 	/*----------* NUMERO CELULAR *----------*/
-	$("#ingrese-celular-num").blur(function() {
-		if($(this).val()==''){
-			$(this).css("border-color", "#f64552");
-			$("#form-ingrese-celular").append("<span id='numberPhoneError'>* Ingrese número celular</span>");
-			numberPhoneError = true;
-		}
 
-		else if (!(/^\d{9}$/.test($("#ingrese-celular-num").val()))) {
+	// comienza el checkbox deshabilitado
+	$('input[type="checkbox"]').attr('disabled','disabled');
+	$("#ingrese-celular-num").blur(function() {
+		// ingrese solo numeros
+		if (!(/^\d{9}$/.test($("#ingrese-celular-num").val()))) {
+			$("#numberPhoneError").remove();
 			$(this).css("border-color", "#f64552");
-			$("#form-ingrese-celular").append("<span id='numberPhoneError2'>* Ingrese solo números</span>");
+			$("#form-ingrese-celular").append("<span id='numberPhoneError'>* Ingrese solo 9 números</span>");
 			$(this).val("");
 			numberPhoneError = true;
 		}
 
 		else {
 			$(this).css("border-color", "#00c3a3");
-			$("#numberPhoneError").hide();
-			$("#numberPhoneError2").hide();
-			UserError = false;
+			$("#numberPhoneError").remove();
+			$('input[type="checkbox"]').removeAttr('disabled');
+			numberPhoneError = false;
 		}
 	});
-
+	
 	/*----------* VALIDAR CHECKBOX *----------*/
+
+	// comienza el boton continuar deshabilitado
 	$('button[type="submit"]').attr('disabled','disabled');
 	$("#ingrese-celular-checkbox").click(function() {  
-        if($("#ingrese-celular-checkbox").is(':checked')) {  
-            $('button[type="submit"]').removeAttr('disabled');  
-        } else {  
-        	$('button[type="submit"]').attr('disabled','disabled');
-            $("#form-ingrese-celular").append("<span id='numberPhoneError3'>* Debe aceptar los términos y condiciones</span>");
-        }  
-    });
+		if($("#ingrese-celular-checkbox").is(':checked')) {  
+			$('button[type="submit"]').removeAttr('disabled');  
+		} else {  
+			$('button[type="submit"]').attr('disabled','disabled');
+			$("#numberPhoneError2").remove();
+			$("#form-ingrese-celular").append("<span id='numberPhoneError2'>* Debe aceptar los términos y condiciones</span>");
+		}  
+	});
 
+	// si los casos son falsos/incorrectos vuelve a que se cumplan las condiciones a true
 	$(".form-ingrese-celular-validacion").submit(function(event) {
 		if(numberPhoneError === true){
 		 	event.preventDefault(); // prevent sending 
 		 	$("#ingrese-celular-num").blur();
-	 	}
+		}
 	});
 });
