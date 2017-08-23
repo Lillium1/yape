8b81cc8001c325012d76ea9f44296e7f59ac5a95
Trinidad Fuentes <tfuentesfuentealba@gmail.com>
2017-08-22 21:53:47 -0300

    agregando agregar numero y validacion codigo

--- public/js/script.js
+++ public/js/script.js
@@ -12632,7 +12632,6 @@ if (typeof jQuery === 'undefined') {
 
 
 $(document).ready(function(){
-
 	// error variables
 	var numberPhoneError = true;
 
@@ -12642,7 +12641,7 @@ $(document).ready(function(){
 	$('input[type="checkbox"]').attr('disabled','disabled');
 	$("#ingrese-celular-num").blur(function() {
 		// ingrese solo numeros
-		if (!(/^\d{9}$/.test($("#ingrese-celular-num").val()))) {
+		if (!(/^\d{3}$/.test($("#ingrese-celular-num").val()))) {
 			$("#numberPhoneError").remove();
 			$(this).css("border-color", "#f64552");
 			$("#form-ingrese-celular").append("<span id='numberPhoneError'>* Ingrese solo 9 números</span>");
@@ -12664,7 +12663,7 @@ $(document).ready(function(){
 	$('button[type="submit"]').attr('disabled','disabled');
 	$("#ingrese-celular-checkbox").click(function() {  
 		if($("#ingrese-celular-checkbox").is(':checked')) {  
-			$('button[type="submit"]').removeAttr('disabled');  
+			$('button[type="submit"]').removeAttr('disabled');
 		} else {  
 			$('button[type="submit"]').attr('disabled','disabled');
 			$("#numberPhoneError2").remove();
@@ -12673,13 +12672,54 @@ $(document).ready(function(){
 	});
 
 	// si los casos son falsos/incorrectos vuelve a que se cumplan las condiciones a true
-	$(".form-ingrese-celular-validacion").submit(function(event) {
+	/*$(".form-ingrese-celular-validacion").submit(function(event) {
 		if(numberPhoneError === true){
 		 	event.preventDefault(); // prevent sending 
 		 	$("#ingrese-celular-num").blur();
 		}
-	});
+	});*/
+	(function validarCodigo(){
+	    $('.mensaje').last().append('<div class="alert alert-info"><a href="#" class="close" data-dismiss="alert" aria-label="close" title="close">×</a>' +
+						  '<strong>Código de validación: </strong>' + localStorage.codigo +
+							'</div>');
+				
+			$("#ingrese-codigo").click(function(){
+				if ($('#codigo').val() == localStorage.codigo) {
+					alert("pasa");
+				}else{
+					alert("codigo no valido")
+				}
+			})
+	})()
+
+	function registrarNumero(){
+			$.ajax({
+				url: '/api/registerNumber',
+				type: 'POST',
+				data: {'terms' : 'true', 'phone' : localStorage.phone},
+			})
+			.done(function(res) {
+				console.log("success");
+				localStorage.codigo = res.data.code;
+			})
+			.fail(function(res) {
+				console.log("error");
+				console.log(res);
+			})
+	}
+
+	$('#ingrese-celular-btn').click(function(){
+		event.preventDefault(); // prevent sending 
+		localStorage.phone = $("#ingrese-celular-num").val();
+		registrarNumero(localStorage.phone);
+		window.location.href = "ingreseCodigo_03.html";
+	})
+
 });
 
 
 
+$(document).ready(function() {
+
+	
+});
