d32c273c1f88f96a7dfd840faf0fdf78eb906dab
Melissa <melissa.pacheco.martinez@gmail.com>
2017-08-24 11:36:22 -0300

    pantalla final maketa

--- assets/js/claveTarjeta_06.js
+++
@@ -1,8 +0,0 @@
-$("#m_crearusuario").hide();
-
-$("#m_inputt").click(function(ev){
-        ev.preventDefault();
-        if($("#m_inputt").val() != "" || $("#m_inputt").length > 4){
-            $("#m_crearusuario").show();
-         }
-    });
